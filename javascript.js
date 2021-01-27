9d56d75d7c046db4c83869b5269d9c7a3eea5e9d
iths-viveka-sjolund <post@vivekas.com>
2021-01-27 19:21:23 +0100

    JS

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
 function sendFeedback(event){
     event.preventDefault()
+    
+    var formData = event.srcElement;
+
+   var thankYouHeader = document.getElementById("thankYou");
+   thankYouHeader.innerText = "Tack" + " " + formData.name.value + ", " +"ditt meddelande har skickats.";
 
 }
